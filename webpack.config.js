52af7387296b81f3535f2515923c5a915dc5611f
yaroslavn <yaroslav.n@open-tag.com>
2025-12-30 14:03:38 +0200

    webpack initial: added plugins, modules, optimization.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,51 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.export = {
+module.exports = {
     mode: 'development',
     entry: {
-        filename: path.resolve(__dirname, 'src/index.js')
+        main: './src/index.js',
+        vendor: './src/vendor.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
+        filename: 'js/[name].[contenthash].js',
+
+    },
+    optimization: {
+        splitChunks: {
+        chunks: 'all',
+        },
+    },
+    plugins: [
+        new HTMLWebpackPlugin({ template: './index.html' }),
+        new CleanWebpackPlugin()
+
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i, // Правило для зображень
+                type: 'asset/resource', // Генерує URL та виводить файл у директорію
+                generator: {
+                    filename: 'assets/images/[name].[hash][ext]', // Шаблон імені файлу для зображень
+                }
+            },
+            {
+                test: /\.(woff|woff2|ttf|eot)$/i, // Правило для шрифтів
+                type: 'asset/resource', // Генерує URL та виводить файл у директорію
+                generator: {
+                    filename: 'assets/fonts/[name].[hash][ext]', // Шаблон імені файлу для шрифтів
+                }
+            },
+        ]
     }
 }
